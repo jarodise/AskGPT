be9a428915b42c8825fa804f1cf7bcb9fab99161
jarodise <jarodise@gmail.com>
2024-12-13 18:42:34 +0800

    update configuration.lua

---
+++ configuration.lua
@@ -0,0 +1,20 @@
+local CONFIGURATION = {
+  -- Replace with your actual API key
+  api_key = "AIzaSyCmmdUR1k_fQOZYm0-Wopz54NdTeHU4icU",
+
+  -- Replace with your choice of model
+  model = "gemini-2.0-flash-exp",
+
+  -- API endpoint for Gemini (with :generateContent)
+  api_endpoint = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-exp:generateContent",
+
+  -- Default system prompt used when asking ChatGPT a question
+  -- It can be overridden by setting a custom prompt in the plugin settings
+  prompt1 = "translate to Spanish.",
+
+  -- Default system prompt used when asking ChatGPT a question
+  -- It can be overridden by setting a custom prompt in the plugin settings
+  prompt2 = "translate to Chinese.",
+}
+
+return CONFIGURATION
